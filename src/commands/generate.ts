74595a6ffb0048ba2604be057bbd39d08336a317
Halil Savaşcı <halilsavasci32@gmail.com>
2022-01-19 23:59:48 +0300

    file watch support for rio g

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -3,9 +3,14 @@ import {ProjectManager} from "../lib/ProjectManager";
 import afterCommand from "./AfterCommand";
 import {CommandModule} from "yargs";
 import Listr from "listr";
+import chokidar from "chokidar"
+import {Ignore} from "../lib/Ignore";
+import path from "path";
+import chalk from "chalk";
+import {PROJECT_RIO_CLASS_FILE} from "../config";
 
 interface Input extends GlobalInput {
-
+    watch: boolean
 }
 
 interface TaskContext {
@@ -18,7 +23,16 @@ module.exports = {
     description: `Generate rio class files
      Usage: rio generate`,
     aliases: ['g'],
-    handler: async () => {
+    builder: yargs => {
+        yargs.options('watch', {
+            type: 'boolean',
+            default: false,
+            boolean: true,
+            describe: 'Watch file changes',
+        })
+        return yargs
+    },
+    handler: async (args) => {
         const tasks = new Listr([
             {
                 title: 'Generate & Save Rio Files',
@@ -27,6 +41,33 @@ module.exports = {
                 }
             }
         ])
+
+        if (args.watch) {
+            let count = 1
+            await new Promise((resolve, reject) => {
+                chokidar.watch(process.cwd()).on("all", (event, _path, stats) => {
+                    if (event === "addDir" || event === "add") return false
+                    if (!_path || _path === '') return false
+
+                    const relativePath = path.relative(process.cwd(), _path)
+
+                    if (path.parse(_path).base === PROJECT_RIO_CLASS_FILE || Ignore.isIgnored(relativePath))
+                        return false
+
+                    process.stdout.clearLine(-1)
+                    process.stdout.clearLine(0)
+                    process.stdout.clearLine(1)
+                    process.stdout.cursorTo(0)
+                    process.stdout.write(`${chalk.bold.gray(relativePath) + ' ' + chalk.bold.greenBright(count)} `)
+                    ProjectManager.generateAndSaveRioFiles().then(d => {
+                        process.stdout.write(chalk.bold.greenBright('Done'))
+                    }).catch(e => {
+                        console.error(e)
+                    })
+                    ++count
+                })
+            })
+        }
 
         await tasks.run()
 
