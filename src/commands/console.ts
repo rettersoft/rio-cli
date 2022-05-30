1adcedc029a530c7cec034384415534e62230673
Halil Savaşcı <halilsavasci32@gmail.com>
2022-05-30 09:22:17 +0300

    rio console command

---
+++ src/commands/console.ts
@@ -0,0 +1,29 @@
+import {GlobalInput} from "./ICommand";
+import afterCommand from "./AfterCommand";
+import {CommandModule} from "yargs";
+import {RIO_CLI_ROOT_DOMAIN, RIO_CLI_STAGE} from "../config";
+import {execSync} from "child_process";
+import {Project} from "../lib/Project";
+
+interface Input extends GlobalInput {
+    out: string
+    open: boolean
+    "include-version": boolean
+    "rio-file-path"?: string
+}
+
+module.exports = {
+    command: 'console',
+    description: `Open project console`,
+    aliases: ['con'],
+    builder: yargs => {
+        return yargs
+    },
+    handler: async (args) => {
+        const config = Project.getProjectRioConfig()
+
+        execSync('open ' + `https://${RIO_CLI_STAGE === 'PROD' ? 'c' : 'ct'}.${RIO_CLI_ROOT_DOMAIN}/project/${config.projectId}/Dashboard`)
+        afterCommand()
+    }
+} as CommandModule<Input, Input>
+
